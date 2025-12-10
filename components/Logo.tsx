5e2df3949eb63bdc486ff5bc8dd67a0541a70490
janekskr <janeczek.skrobot@gmail.com>
2025-12-10 09:51:44 +0100

    feat: logo change

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -7,7 +7,7 @@ export default function Logo({ className }: { className?: string }) {
   return (
     <div className={`flex items-center gap-2`}>
       <Image src={logo} alt="VeroAI" width={32} height={32} />
-      <span className={cn("text-xl font-bold text-primary", className)}>
+      <span className={cn("text-xl font-bold text-black", className)}>
         VeroAI
       </span>
     </div>
